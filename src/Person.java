029120df8a2441b482ee78c681e59500bc9e62d8
Reza Niyakan <RezaNiyakan@gmail.com>
2022-01-12 12:24:29 -0600

    Fixed another Annotation issue

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,7 @@
 /**
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
-public class Person {
+public abstract class Person {
     protected String firstName;
     protected String lastName;
 
@@ -9,8 +9,10 @@ public class Person {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
+    @Deprecated
     public String fullName() {
         return String.format("%s %s", firstName, lastName);
     }
+    @Deprecated
+    public abstract String sortName();
 }
